0bcc8634c3de16d800c78bdec74564a6798379dd
Jan Redmann <janredmann1@gmail.com>
2021-07-23 14:18:00 +0200

    add serve index html

--- app.js
+++ app.js
@@ -102,6 +102,7 @@ app.use("/api/auth", auth);
 const path = require('path');
 app.use(express.static(path.join(__dirname, "/client/build")));
 
+
 app.use((req, res) => {
   // If no routes match, send them the React HTML.
   res.sendFile(__dirname + "/client/build/index.html");
